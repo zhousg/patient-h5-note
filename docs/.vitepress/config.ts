f3a63458d10d8a29b42aad7536097c10eede7bc3
zhousg <zhoushugang@itcast.cn>
2022-09-26 21:29:45 +0800

    copyright

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     ],
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2022-present Allen Zhou'
+      copyright: 'Copyright © 2022-present Shugang Zhou 传智教育'
     },
     lastUpdatedText: '更新',
     sidebar: {
