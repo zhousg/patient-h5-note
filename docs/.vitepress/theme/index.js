f5a6e778a0b042c2a225ae8b02dc2afec2026919
zhousg <zhoushugang@itcast.cn>
2022-10-11 07:50:42 +0800

    test

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,16 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
-// import Antd from 'ant-design-vue';
 import './custom.css'
-// import 'ant-design-vue/dist/antd.css';
 
 import SuperDoctor from './components/SuperDoctor.vue'
-import CodeEditor from './components/CodeEditor.vue'
 
 export default { 
   ...DefaultTheme,
   enhanceApp({ app }) {
-    // app.use(Antd)
     app.component('SuperDoctor', SuperDoctor)
-    app.component('CodeEditor', CodeEditor)
   }
 }
